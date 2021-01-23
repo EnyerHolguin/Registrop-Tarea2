944173c14d3d0dda5ed282b620880c427bae200c
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-01-23 17:41:40 -0400

    Registro listo

---
+++ RegistroP/Controllers/PersonasController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroP.Data;
+using RegistroP.Models;
+using System;
+
+namespace RegistroP.Controllers
+{
+    public class PersonasController
+    {
+        public bool Insertar(Personas Persona)
+        {
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            if (Persona.PersonaId == 0)
+            {
+
+                paso = Guardar(Persona);
+            }
+            else
+            {
+
+                paso = Modificar(Persona);
+            }
+
+            return paso;
+        }
+
+        public static bool Guardar(Personas Persona)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Personas.Add(Persona);
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public static bool Modificar(Personas Persona)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+
+            try
+            {
+
+                contexto.Entry(Persona).State = EntityState.Modified;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public bool Eliminar(int Id)
+        {
+
+            bool paso = false;
+            Contexto contexto = new Contexto();
+            Personas Persona = contexto.Personas.Find(Id);
+
+            try
+            {
+
+                contexto.Entry(Persona).State = EntityState.Deleted;
+                paso = contexto.SaveChanges() > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return paso;
+        }
+
+        public Personas Buscar(int Id)
+        {
+
+            Contexto contexto = new Contexto();
+            Personas Persona = new Personas();
+
+            try
+            {
+
+                Persona = contexto.Personas.Find(Id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            finally
+            {
+
+                contexto.Dispose();
+            }
+
+            return Persona;
+        }
+    }
+}
