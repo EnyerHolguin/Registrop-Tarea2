0499a8e6b40c44d87d2aa90bb4a6e6c74678e7b2
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-01-23 16:54:54 -0400

    Creando la migracion

--- RegistroP/Data/Contexto.cs
+++ RegistroP/Data/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RegistroP.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace RegistroP.Data
 {
     public class Contexto : DbContext
     {
+        public DbSet<Personas> Personas { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite(@"Data Source = Database/RegistroP");
