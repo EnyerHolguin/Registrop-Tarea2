9186d27a0c33a789618a72cce3741591be3f9d47
Enyer De Jesus Holguin Toribio <enyer_holguin@ucne.edu.do>
2021-01-27 19:39:27 -0400

    se agrego un constructor en el contexto closes #1.

--- RegistroP/Data/Contexto.cs
+++ RegistroP/Data/Contexto.cs
@@ -9,10 +9,10 @@ namespace RegistroP.Data
 {
     public class Contexto : DbContext
     {
-        public DbSet<Personas> Personas { get; set; }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite(@"Data Source = Database/RegistroP");
-        }
+            
+            public DbSet<Personas> Personas { get; set; }
+
+            public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+   
     }
 }
